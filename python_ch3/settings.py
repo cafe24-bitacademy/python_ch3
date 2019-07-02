a72424c57cbc402967c01c4595d0f2b1d0c13571
kickscar <kickscar@gmail.com>
2019-07-02 14:47:32 +0900

    db setting

--- python_ch3/settings.py
+++ python_ch3/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'webdb',
         'USER': 'webdb',
         'PASSWORD': 'webdb',
-        'HOST': '192.168.0.39',
+        'HOST': '192.168.1.250',
         'PORT': 5432
     }
 }
